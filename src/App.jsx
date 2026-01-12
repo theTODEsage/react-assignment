a3085916b27cb5a8a9bade1f19cdb03c80304687
theTODEsage <menarebrave7878@gmail.com>
2026-01-12 15:29:22 +0600

    loaded data

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,19 @@ import Counter from './Components/Counter/Counter'
 import Nav from './Components/Navbar/Nav'
 import Tickets from './Components/Tickets/Tickets'
 
+const fetchData = async() =>{
+  const res = await fetch('service.json')
+  return res.json()
+}
+const promise = fetchData()
+
 function App() {
 
   return (
     <>
       <Nav></Nav>
       <Counter></Counter>
-      <Tickets></Tickets>
+      <Tickets promise={promise} ></Tickets>
     </>
   )
 }
--- src/Components/Tickets/Tickets.jsx
+++ src/Components/Tickets/Tickets.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { use } from 'react';
+import Ticket from '../Ticket/Ticket';
 
-const Tickets = () => {
+const Tickets = ({ promise }) => {
+
+    const ticketList = use(promise)
+
     return (
-        <div>
-            
+        <div className='px-20 flex items-center justify-between'>
+            <div>
+                <h2>Customer Tickets</h2>
+
+                {
+                    ticketList.map((ticket, index)=> <Ticket key={index} ticket={ticket}></Ticket>)
+                }
+
+
+            </div>
+            <div>
+                <h2>Task status</h2>
+            </div>
         </div>
     );
 };
